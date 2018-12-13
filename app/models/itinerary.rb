1ecc3935bb80aa85b145e6d5d5f7ef0876f4918c
Artur Galyamov <redex2000@gmail.com>
2018-12-13 14:10:28 +0500

    refs #3039 другой способ

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -2,4 +2,8 @@ class Itinerary < ApplicationRecord
   belongs_to :expedition
   has_many :itinerary_entries
   has_many :planets, through: :itinerary_entries
+
+  def route_length
+    self[:route_length].to_f > 0 ? self[:route_length].to_f : planets.sum(:distance)
+  end
 end
