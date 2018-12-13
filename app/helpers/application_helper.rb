13d3f4fbaa0ff212e90c1b548dd280eb82707eb7
Artur Galyamov <redex2000@gmail.com>
2018-12-13 12:15:46 +0500

    top_menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def context_menu(links)
+    menu = controller.render_to_string partial: "shared/dropdown_links", collection: links
+    content_tag :div, menu.html_safe, class: 'dropdown-menu', aria: { labelledBy: 'expedition-dropdown' }
+  end
+
+
+  def expedition_links
+    [
+        ['Список', expeditions_path],
+        ['Корабли', '#'],
+        ['Команда', '#'],
+    ]
+  end
 end
